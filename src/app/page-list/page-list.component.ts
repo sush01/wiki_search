09aa9ba0414322cab711046586b02a518fe2d0fe
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-09 22:32:19 +0200

    create pagelist component

---
+++ src/app/page-list/page-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list',
+  imports: [],
+  templateUrl: './page-list.component.html',
+  styleUrl: './page-list.component.css'
+})
+export class PageListComponent {
+
+}
